949924bffdb1a510771c6a7bcee55bc20b5a181d
Rjndrkha <75615789+Rjndrkha@users.noreply.github.com>
2023-03-08 19:44:58 +0700

    Done ALL Fragment & Navigation

---
+++ app/src/main/java/com/dicoding/rjndrkha/myflexiblefragment/DetailCategoryFragment.kt
@@ -0,0 +1,51 @@
+package com.dicoding.rjndrkha.myflexiblefragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.Navigation
+import com.dicoding.rjndrkha.myflexiblefragment.databinding.FragmentDetailCategoryBinding
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [DetailCategoryFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class DetailCategoryFragment : Fragment() {
+
+    private var _binding: FragmentDetailCategoryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentDetailCategoryBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val dataName = DetailCategoryFragmentArgs.fromBundle(arguments as Bundle).name
+        val dataDescription = DetailCategoryFragmentArgs.fromBundle(arguments as Bundle).stock
+
+        binding.tvCategoryName.text = dataName
+        binding.tvCategoryDescription.text = "Stock : $dataDescription"
+
+        binding.btnProfile.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_detailCategoryFragment_to_homeFragment)
+        )
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
